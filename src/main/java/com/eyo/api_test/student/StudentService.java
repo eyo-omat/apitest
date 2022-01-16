219d877b1dfdffa43f731444b17fdc8021082aeb
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-01-16 16:52:13 +0000

    :clown_face: Updated post to save student with business logic of email validation

--- src/main/java/com/eyo/api_test/student/StudentService.java
+++ src/main/java/com/eyo/api_test/student/StudentService.java
@@ -2,9 +2,8 @@ package com.eyo.api_test.student;
 
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -20,6 +19,10 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> s = studentRepository.findStudentByEmail(student.getEmail());
+        if (s.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
     }
 }
