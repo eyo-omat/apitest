d3171177d1c907d08fc59f96f9da49fe4e85f618
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-01-16 16:10:14 +0000

    :clown_face: Added Entity annotation to Student model

--- src/main/java/com/eyo/api_test/student/Student.java
+++ src/main/java/com/eyo/api_test/student/Student.java
@@ -2,13 +2,26 @@ package com.eyo.api_test.student;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     @NonNull
     private String name;
