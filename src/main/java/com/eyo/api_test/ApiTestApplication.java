e84ec3d6283ec234ca2d396877bf4c584f68b3e5
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-01-16 14:33:07 +0000

    :clown_face: Added Hello world function to test RestController

--- src/main/java/com/eyo/api_test/ApiTestApplication.java
+++ src/main/java/com/eyo/api_test/ApiTestApplication.java
@@ -2,12 +2,21 @@ package com.eyo.api_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class ApiTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiTestApplication.class, args);
     }
 
+    @GetMapping
+    public List<String> hello() {
+        return List.of("hello",  "World");
+    }
 }
