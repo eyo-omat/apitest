7610ea9a964e93c7f19943469cfc722ed2a844df
Eyioyo Omatsola <eyo.omat@gmail.com>
2022-01-16 16:26:39 +0000

    :clown_face: Added StudentConfig to populate Database with test data

---
+++ src/main/java/com/eyo/api_test/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.eyo.api_test.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student kojo = new Student(
+                    "Kojo",
+                    38,
+                    LocalDate.of(1999, Month.AUGUST, 7),
+                    "kj@mail.com"
+
+            );
+            Student eyo = new Student(
+                    "Eyo",
+                    38,
+                    LocalDate.of(1979, Month.AUGUST, 7),
+                    "eeyo@mail.com"
+
+            );
+            repository.saveAll(
+                    List.of(kojo, eyo)
+            );
+
+        };
+    }
+}
